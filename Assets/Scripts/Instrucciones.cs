7e765c46656155f702b23b5e312a792842d86770
AdrichanFE <adrielpelao_xty@hotmail.com>
2025-02-10 14:38:36 +0100

    EscapeRoomTerminado

---
+++ Assets/Scripts/Instrucciones.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instrucciones : MonoBehaviour
+{
+   //Comenzaremos encerrados en una mina, avanzaremos un poco por el tunel guiandonos con la mano en la pared, el tunel se dividira en dos caminos.
+   //Decides ir por el de la derecha, ya que alcanzas a ver un pequeño destello.
+   // Llegas a una sala grande iluminada por dos faroles, decides coger el farol de la entrada y acercarte a investigar, a ver si descubres alguna pista sobre como salir de alli.
+   //Te acercas al otro farol y encuentras un mechero, cuerda y un libro, tambien hay un cofre militar y un pico, pero lo que mas te llama la atencion es esa puerta tan misteriosa.
+   //Pide un codigo, el cual no tienes, asi que te toca seguir investigando, vuelves con el libro y ves que hay algo escrito.
+   // "Entre rocas y sombras se esconde el fulgor, solo un golpe certero revelara su color. No busques arriba, ni mires atras, donde el pico resuena, la clave hallaras, si con tino lo usas, la senda veras"
+   //Como ese acertijo hace referencia al pico que hay cerca decides cogerlo, y aunque podrias intentar abrir la caja militar, esta hecha de metal con lo que sera un esfuerzo inutil,asi que decides investigar un poco la mina.
+   //Siguiendo el tunel izquierdo que dejaste atras antes, llegas a otra bifurcacion, vas por la derecha, y llegas a una sala en la que hay rocas con piedras preciosas.
+   //Investigas un poco y encuentras otra nota. "Brillan en la oscuridad, pero solo tres serviran, una del fuego, otra del mar y una que el bosque vera. Si eliges bien y con fuerza las tomas, mas adelante las sombras abiran".
+   //Picas en las menas de piedras preciosas, y consigues 3 gemas, de rubi, zafiro y esmeralda, y las guardas como dice el acertijo.
+   //Ahora te dirijes por el otro camino que no has investigado, y llegas a otra sala, en la que parece que no  hay nada. Pero investigando un poco mas encuentras otro acertijo.
+   //"Tres luces llevas, tres sombras ves, donde brillaban, ahora al reves. Como un reflejo en cristal oscuro, lo que fue derecho, hoy es zurdo. Si el orden inviertes sin titubear, el secreto oculto se dejara hallar"
+   //Invstigas un poco la sala y descubres tres huecos en las paredes, tienes que introducir las gemas en ellos, en el mismo lugar donde las picaste en la otra sala, pero usando los huecos como si estuvieran invertidos por un espejo.
+   //Despues de colocarlos, escuchas un ruido y una de las paredes se desplaza, dejando a la vista un hueco, y dentro una tarjeta de seguridad
+   //Tomas la tarjeta, y regresas a la sala de la puerta extraña, intentas usar la tarjeta para el escaner dela puerta pero no funciona, asi que lo usas en la caja militar y esta se abre, en el interior encuentras una nota en la que esta escrito un codigo.
+   ////Lo usas en la puerta, y esta se abre, la sala que hay detras de la puerta, esta totalmente derruida, y encuentras unos cartuchos de dinamita en el suelo.
+   ///Te llevas uno, junto al mechero y te dirijes a la parte del tunel donde despertaste y estaba bloqueada.
+   ///enciendes la dinamita, te alejas lo suficente para salir del radio de peligro, y despues de la explosion, y que el tunel no se haya venido abajo, te asomas para ver que hay luz al final del tunel, escapando por fin de aquella mina.
+}
